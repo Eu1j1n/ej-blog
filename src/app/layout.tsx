8110d6dc9c674ac1a7b38bbc33f10197f5455654
김의진 <dmlwls7094@naver.com>
2025-04-21 00:32:43 +0900

    feat: og 이미지 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,14 @@ export const metadata: Metadata = {
     description: "개발자 김의진의 기술 블로그입니다.",
     type: "website",
     locale: "ko_KR",
+    images: [
+      {
+        url: "https://ej-blog.vercel.app//kimeuijin.jpg",
+        width: 1200,
+        height: 630,
+        alt: "EJ Blog 대표 이미지",
+      },
+    ],
   },
 };
 
